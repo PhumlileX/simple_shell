19f6bec346984de57636c2309f1ca2d968a5f5ac
Phumlile <you@pxnonkosi@gmail.com>
2023-04-27 00:21:01 -0700

    updated

--- alias_builtin.c
+++ alias_builtin.c
@@ -1,4 +1,10 @@
 #include "shell.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+void set_alias(char *var_name, char *value);
+void print_alias(alias_t *alias);
 
 /**
  * shellby_alias - Builtin command that either prints all aliases, specific
